193dfc32535cdcb0874b11646b10bea352d9a61b
Matt Friedman <maf675@gmail.com>
2014-10-20 09:55:04 -0700

    Remove unused javascript

--- styles/all/template/tiny_mce/scripts.js
+++ styles/all/template/tiny_mce/scripts.js
@@ -1,24 +1,3 @@
-(function($) { // Avoid conflicts with other libraries
-
-'use strict';
-
-$(function() {
-
-	$.fn.toggleBoxes = function(target) {
-		$(this).on('click', function () {
-			if ($(this).is(':checked')) {
-				$(target).prop('checked', false);
-			}
-		});
-	};
-
-	$('.html-on').toggleBoxes('.html-off');
-	$('.html-off').toggleBoxes('.html-on');
-
-});
-
-})(jQuery); // Avoid conflicts with other libraries
-
 	tinymce.init({
 		selector: "textarea.mceEditor",
 		relative_urls: false,
@@ -28,4 +7,4 @@ $(function() {
 			"insertdatetime media table contextmenu paste textcolor"
 		],
 		toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | fore-color | link image | forecolor",
-	});	
+	});
